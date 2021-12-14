3daa9063fe0eaaadf27aaba4f8af44c89a1819d5
Peter Kohler <peter@oneprismatic.com>
2021-12-14 12:46:13 -0600

    first push

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,30 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: "Hi, I'm Pete Kohler",
+    description: "web dev portfolio",
+    copyright: "This website is copyright 2021 Pete Kohler",
+    contact: "peterkohler95@gmail.com",
+  },
 }
---
+++ src/pages/projects/index.js
@@ -0,0 +1,64 @@
+import React from "react"
+import Layout from "../../components/Layout"
+import * as styles from "../../styles/projects.module.css"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const Projects = ({ data }) => {
+  console.log(data)
+  const projects = data.projects.nodes
+  const contact = data.contact.siteMetadata.contact
+
+  return (
+    <Layout>
+      <div className={styles.portfolio}>
+        <h2>Portfolio</h2>
+        <h3>Projects & Websites I've Created</h3>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link to={"/projects/" + project.frontmatter.slug}>
+              <div>
+                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+        <p>Email me at {contact}</p>
+      </div>
+    </Layout>
+  )
+}
+
+export default Projects
+
+// export page query
+export const query = graphql`
+  query ProjectsPage {
+    projects: allMarkdownRemark(
+      sort: { order: DESC, fields: frontmatter___date }
+    ) {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+    contact: site {
+      siteMetadata {
+        contact
+      }
+    }
+  }
+`
